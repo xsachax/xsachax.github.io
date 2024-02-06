930851fa05be2c472be05ba4aac4943563e033d4
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-02-05 22:19:32 -0500

    feat: loader

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,11 @@
-import { Suspense } from "react";
+import { useState } from "react";
 import styles from "./Hero.module.css";
 import { t } from "../../i18n";
 import "../../global.css";
 
 function Hero() {
+  const [frameLoaded, setFrameLoaded] = useState(false);
+
   return (
     <div className={styles["hero"]}>
       <div className={styles["hero-content"]}>
@@ -11,19 +13,20 @@ function Hero() {
         <p>{t("hero.subtitle")}</p>
       </div>
       <div className={styles["spline-container"]}>
-        <Suspense fallback={<p>Loading...</p>}>
-          <iframe
-            id="frame1"
-            name="frame1"
-            alt="Desk Scene"
-            title="Desk Scene"
-            className={styles["spline"]}
-            src="https://my.spline.design/untitled-34d9f889fb20377288d352814dd7c074/"
-            frameBorder="0"
-            width="100%"
-            height="100%"
-          ></iframe>
-        </Suspense>
+        {frameLoaded ? <></> : <div className={styles["loader"]}></div>}
+        <iframe
+          id="frame1"
+          name="frame1"
+          alt="Desk Scene"
+          title="Desk Scene"
+          className={styles["spline"]}
+          src="https://my.spline.design/untitled-34d9f889fb20377288d352814dd7c074/"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          onLoad={() => setFrameLoaded(true)}
+        ></iframe>
+
         <div className={styles["fade"]}></div>
         <div className={styles["button-mask"]}></div>
       </div>
