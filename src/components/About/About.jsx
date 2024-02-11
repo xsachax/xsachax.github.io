000b1b0839ec851dfe3058c4b43ffcb0cb002449
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-02-11 15:04:23 -0500

    feat: type animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,5 @@
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 import styles from "./About.module.css";
 import { t } from "../../i18n";
 import "../../global.css";
@@ -7,7 +9,32 @@ function About() {
     <div className={styles["about"]}>
       <div className={styles["about-container"]}>
         <div className={styles["about-content"]}>
-          <h1>{t("about.title")}</h1>
+          <h1>
+            {t("about.title")}
+            &nbsp;
+            <TypeAnimation
+              sequence={[
+                ". . . 🖥️",
+                2000,
+                " . . . 🎨",
+                2000,
+                ". . . 🚀",
+                2000,
+                " . . . 🤠",
+                2000,
+                ". . . 🏔️",
+                2000,
+                " . . . 🏐",
+                2000,
+              ]}
+              wrapper="span"
+              speed={5}
+              repeat={Infinity}
+              className={styles["type-animation"]}
+              style={{ color: "var(--shade-3)" }}
+            />
+          </h1>
+
           <p>{t("about.p1")}</p>
           <p>{t("about.p2")}</p>
           <p>{t("about.p3")}</p>
--- src/locales/fr.ts
+++ src/locales/fr.ts
@@ -8,7 +8,7 @@ export default {
     email_alt: "Email",
   },
   about: {
-    title: "À propos de moi 🦕",
+    title: "À propos de moi",
     p1: "Je suis Sacha, un étudiant de premier cycle en Génie Logiciel à l'Université d'Ottawa 🎓, au Canada. 🇨🇦",
     p2: "Je souhaite poursuivre une carrière en développement AR/VR 🥽, et j'adore utiliser mon travail en logiciel pour aider les autres. 🌱",
     p3: "Connectez-vous avec moi sur les réseaux sociaux 📱, j'adore parler de développement web, de réalité virtuelle, ou de n'importe quelle autre technologie! 💻",
