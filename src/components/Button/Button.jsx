ea2d604d9d1a5d74b14fc3bf0238b5c095cff215
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-02-18 18:28:35 -0500

    style: button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import styles from "./Button.module.css";
 function Button({ children, onClick }) {
   return (
     <button className={styles["square-button"]} onClick={onClick}>
-      {children}
+      <div className={styles["square-button-content"]}>{children}</div>
     </button>
   );
 }
