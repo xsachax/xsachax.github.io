70165e4bd00f8fdd4efd600bf68f67b5dc15d048
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-02-18 21:05:45 -0500

    feat: add email icon to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import linkedin_card_icon from "../../assets/icons/linkedin_card_icon.svg";
 import github_card_icon from "../../assets/icons/github_card_icon.svg";
 import devpost_card_icon from "../../assets/icons/devpost_card_icon.svg";
+import mail_card_icon from "../../assets/icons/mail_card_icon.svg";
 import styles from "./Footer.module.css";
 import "../../global.css";
 
@@ -46,6 +47,19 @@ function Footer() {
               <img
                 src={devpost_card_icon}
                 alt="Devpost"
+                width="25px"
+                height="25px"
+              />
+            </a>
+            <a
+              href="mailto:sacha.arseneault@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles["link"]}
+            >
+              <img
+                src={mail_card_icon}
+                alt="Email"
                 width="25px"
                 height="25px"
               />
