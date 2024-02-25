3e34b8f3ceb9162f4bfdba743e289ebe1a9a6a9e
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-02-25 18:46:21 -0500

    feat: footer border option

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,78 +20,80 @@ function Footer() {
   return (
     <div>
       <nav className={styles["footer"]}>
-        <div
-          className={styles["footer-content"]}
-          data-aos="fade-right"
-          data-aos-duration="700"
-        >
+        <div className={styles["footer-container"]}>
           <div
-            className={styles["footer-title"]}
-            onClick={() => {
-              window.open(
-                "https://github.com/xsachax/xsachax.github.io",
-                "_blank"
-              );
-            }}
+            className={styles["footer-content"]}
+            data-aos="fade-right"
+            data-aos-duration="700"
           >
-            {t("footer.title")}
-          </div>
-          <div className={styles["footer-links"]}>
-            <a
-              href="https://www.linkedin.com/in/sacha-ars/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles["link"]}
+            <div
+              className={styles["footer-title"]}
+              onClick={() => {
+                window.open(
+                  "https://github.com/xsachax/xsachax.github.io",
+                  "_blank"
+                );
+              }}
             >
-              <img
-                src={linkedin_card_icon}
-                alt="LinkedIn"
-                width="25px"
-                height="25px"
-              />
-            </a>
-            <a
-              href="https://github.com/xsachax"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles["link"]}
-            >
-              <img
-                src={github_card_icon}
-                alt="GitHub"
-                width="25px"
-                height="25px"
-              />
-            </a>
-            <a
-              href="https://devpost.com/xsachax"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles["link"]}
-            >
-              <img
-                src={devpost_card_icon}
-                alt="Devpost"
-                width="25px"
-                height="25px"
-              />
-            </a>
-            <a
-              href="mailto:sacha.arseneault@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles["link"]}
-            >
-              <img
-                src={mail_card_icon}
-                alt="Email"
-                width="25px"
-                height="25px"
-              />
-            </a>
-          </div>
-          <div>
-            <p>{`© Sacha Arseneault ${year}`}</p>
+              {t("footer.title")}
+            </div>
+            <div className={styles["footer-links"]}>
+              <a
+                href="https://www.linkedin.com/in/sacha-ars/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles["link"]}
+              >
+                <img
+                  src={linkedin_card_icon}
+                  alt="LinkedIn"
+                  width="25px"
+                  height="25px"
+                />
+              </a>
+              <a
+                href="https://github.com/xsachax"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles["link"]}
+              >
+                <img
+                  src={github_card_icon}
+                  alt="GitHub"
+                  width="25px"
+                  height="25px"
+                />
+              </a>
+              <a
+                href="https://devpost.com/xsachax"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles["link"]}
+              >
+                <img
+                  src={devpost_card_icon}
+                  alt="Devpost"
+                  width="25px"
+                  height="25px"
+                />
+              </a>
+              <a
+                href="mailto:sacha.arseneault@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles["link"]}
+              >
+                <img
+                  src={mail_card_icon}
+                  alt="Email"
+                  width="25px"
+                  height="25px"
+                />
+              </a>
+            </div>
+            <div>
+              <p>{`© Sacha Arseneault ${year}`}</p>
+            </div>
           </div>
         </div>
       </nav>
